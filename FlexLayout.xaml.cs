50845fd0d746f54996303a1ab4d4f4cc89796be9
DiegoCorrea07 <diego.correa.cumbicus07@outlook.com>
2024-05-23 09:19:23 -0500

    Actualización de Grid, StackLayout y FlexLayout con imágenes.

--- FlexLayout.xaml.cs
+++ FlexLayout.xaml.cs
@@ -1,13 +1,17 @@
+using Microsoft.Maui.Controls;
+
 namespace TallerP2;
 
 public partial class FlexLayout : ContentPage
 {
 	public FlexLayout()
 	{
-		InitializeComponent();
-	}
-    private async void OnButtonClicked(object sender, EventArgs e)
+        InitializeComponent();
+
+    }
+    private async void OnImageTapped(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new AbsoluteLayout());
     }
+  
 }
